0e41c6061dfec919dcc6cbf895b3f52a06e3129a
Belal Sayed <belalsayedali273@gmail.com>
2024-09-26 02:17:37 +0300

    Like Feature
    Like Almost implemented
    1- locate svg by aria label like
    2- text parse the path to reach the clickable button not svg
    3- double click needed
    4- still need to click Esx or X
    
    General Note:
    1- Load html at correct point
    2- driver needs to be passed tp function

--- Selenium_Project/Badry_modified_SoupwithChrome.py
+++ Selenium_Project/Badry_modified_SoupwithChrome.py
@@ -7,6 +7,8 @@ from msedge.selenium_tools import Edge, EdgeOptions
 from bs4 import BeautifulSoup
 import os , time
 
+
+from selenium.webdriver.common.keys import Keys
 global driver
 
 
@@ -76,14 +78,94 @@ def find_parent_rank(parent):
 
 
 driver = webdriver.Chrome()
-driver.get('https://www.youtube.com/')
+driver.get('https://www.instgram.com/')
+time.sleep(10)
+username_field = driver.find_element(By.NAME, "username")
 
+    
+for letter in "bbelal.ali":
+    username_field.send_keys(letter)  # Send each letter
+    time.sleep(0.1)  # Delay between letters
+
+password_field = driver.find_element(By.NAME, "password")
+for letter in "Bolbol00curt":
+    password_field.send_keys(letter)  # Send each letter
+    time.sleep(0.1)  # Delay between letters
+login_button = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div')
+login_button.click()  # Click the Log In button
+time.sleep(10)
+driver.get(f"https://www.instagram.com/hesham_262/")
+time.sleep(10)
+# print("Before Post")
+# time.sleep(5) 
+# html = driver.page_source
+# soup = BeautifulSoup(html,features="html.parser")
+# # testElement = wait("button" ,"aria-label" , "Search with your voice" , 0)
+# testElement = wait("div" ,"role" , "button" , 0)
+# print(testElement)
+# print(get_xpath(testElement))
+# time.sleep(10) 
+most_recent_post = driver.find_element(By.XPATH, "//div[contains(@class, 'aagw')]") # Working # message_button = driver.find_element(By.XPATH, "//div[contains(text(), 'Message')]") # Working
+
+# Click on the most recent post
+most_recent_post.click()
+
+# Wait to see the post after clicking
+time.sleep(5)  # Adjust time as needed for observation
+
+print("After Post")
+time.sleep(5) 
+current_url = driver.current_url
+print("Current URL:", current_url)
+time.sleep(5)
 html = driver.page_source
 soup = BeautifulSoup(html,features="html.parser")
-# testElement = wait("button" ,"aria-label" , "Search with your voice" , 0)
-testElement = wait("button" ,"class" , "yt-spec-button-shape-next" , 1)
+
+# testElement = wait("div" ,"class" , "x1i10hfl x972fbf xcfux6l x1qhh985 xm0m39n x9f619 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x6s0dn4 xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x1ypdohk x78zum5 xl56j7k x1y1aw1k x1sxyh0 xwib8y2 xurb0ha xcdnw81" , 0)
+testElement = wait("svg" ,"aria-label" , "Like" , 0)
+
 print(testElement)
-print(get_xpath(testElement))
+likePAth = get_xpath(testElement)
+print(likePAth)
+time.sleep(5) 
+# try:
+#     driver.find_element(By.XPATH, likePAth).click()
+# except:
+#     print("Not Clickable")
+
+# try:
+#     driver.find_element(By.XPATH, likePAth)
+
+# except:
+#     print("Not Enter")
+
+
+xx = driver.find_element(By.XPATH, "/html/body/div[7]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/div/div")
+time.sleep(5)
+xx.click()
+time.sleep(5)
+
+print("FINALE")
+time.sleep(20)
+xx = driver.find_element(By.XPATH, "/html/body/div[7]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/div/div")
+time.sleep(5)
+xx.click()
+time.sleep(5)
+
+
+
+
+# print("Using Bad Method")
+# time.sleep(15) 
+# driver.get(current_url)
+# time.sleep(10)
+# html = driver.page_source
+# soup = BeautifulSoup(html,features="html.parser")
+# # testElement = wait("button" ,"aria-label" , "Search with your voice" , 0)
+# testElement = wait("div" ,"role" , "button" , 0)
+# print(testElement)
+# print(get_xpath(testElement))
+# time.sleep(5) 
 # Keep the browser open for observation
 input("Press Enter to close this browser...")
     
